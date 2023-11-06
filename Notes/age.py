8a654b24d7b060a1103f3f2dc9abb938d9787294
Ashton Warkentin <ashtonwarkentin@gmail.com>
2023-11-06 09:14:46 -0800

    added assignemnts

---
+++ Notes/age.py
@@ -0,0 +1,6 @@
+age = input("How old are u ")
+new_age = int(age) + 26
+print(f"ur age in 2049 is {new_age}")
+age = input("How old are u ")
+new_age = int(age) + 26
+print(f"ur age in 2049 is {new_age}")
